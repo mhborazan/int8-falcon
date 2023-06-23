d4a579338dcf98286ab610b7f399240bbb5b0d37
Thalia <thaliatelopoulou@gmail.com>
2023-06-23 17:28:44 +0300

    follow button js completed

--- follow-button.js
+++ follow-button.js
@@ -1,1 +1,15 @@
-let followBtn = document.body.querySelector('.btn-follow')
+let followBtn = document.querySelectorAll(".btn-follow");
+let followBtnIcon = document.querySelector('.btn-follow').innerHTML;
+
+followBtn.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    if (e.target.classList.contains("js-follow-btn")) {
+      e.target.classList.remove("js-follow-btn");
+      e.target.innerHTML = followBtnIcon;
+      
+    } else {
+      e.target.classList.add("js-follow-btn");
+      e.target.innerText = "Followed"
+    }
+  });
+});
