d54d4af2b43e768c078a8e18aa52396462bf022d
Carlos Gómez Linares <16gcarlos.93@gmail.com>
2023-06-28 21:05:45 +0200

    followingg button by Thalia

--- posts.js
+++ posts.js
@@ -204,4 +204,19 @@ var hashtagContent = document.getElementsByClassName("hashtagInput");
 focusHashtag[0].addEventListener("click", () => {
   hashtagContent[0].focus();
 });
+//Follow button
+let followBtn = document.querySelectorAll(".btn-follow");
+let followBtnIcon = document.querySelector('.btn-follow').innerHTML;
 
+followBtn.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    if (e.target.classList.contains("js-follow-btn")) {
+      e.target.classList.remove("js-follow-btn");
+      e.target.innerHTML = followBtnIcon;
+      
+    } else {
+      e.target.classList.add("js-follow-btn");
+      e.target.innerText = "Followed"
+    }
+  });
+});
